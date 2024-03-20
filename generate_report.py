9a7aedc183beaa0bd5fdbf13448a5a38ea9edb62
Josep Prades Vidal <103001236+jus25@users.noreply.github.com>
2024-03-20 12:25:20 +0100

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -2,5 +2,17 @@ import os
 from datetime import datetime, timedelta
 from github import Github
 
-print("aaaaaaaaa")
+# Token de acceso personal de GitHub
+github_token = os.getenv('GITHUB_TOKEN')
 
+# Nombre de usuario y nombre del repositorio
+username = 'Troter2'
+repository_name = 'HotelManagementProject'
+
+# Crear una instancia de la clase Github
+g = Github(github_token)
+
+# Obtener el repositorio
+repo = g.get_user(username).get_repo(repository_name)
+
+print(repo)
