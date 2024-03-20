ec7a3d1c9fe9dfa4b047c3857beeaef83848ba91
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-03-20 12:32:33 +0100

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -3,16 +3,15 @@ from datetime import datetime, timedelta
 from github import Github
 
 # Token de acceso personal de GitHub
-github_token = os.getenv('TOKEN')
+github_token = os.getenv('GITHUB_TOKEN')
 
 # Nombre de usuario y nombre del repositorio
-username = 'Troter2'
-repository_name = 'HotelManagementProject'
+repository_name = 'Troter2/HotelManagementProject'
 
 # Crear una instancia de la clase Github
 g = Github(github_token)
 
 # Obtener el repositorio
-repo = g.get_user(username).get_repo(repository_name)
+repo = g.get_repo(repository_name)
 
 print(repo)
