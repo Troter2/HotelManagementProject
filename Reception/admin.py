232795d7d35f33e1f60e9ef79bb4a2997591efe3
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-03-27 23:58:15 +0100

    Login decorations

--- Reception/admin.py
+++ Reception/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 
 from Reception.models import RoomReservation
-
-# Register your models here.
-
-admin.site.register(RoomReservation)
-
 from Reception.models import Room, RoomType
 
 # Register your models here.
+
+
+admin.site.register(RoomReservation)
 admin.site.register(Room)
 admin.site.register(RoomType)
