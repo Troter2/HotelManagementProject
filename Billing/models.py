cd07d738b463934a6f1b217eef4ab2deae49d9d3
Juanca192001 <joancarlesturegano@gmail.com>
2024-03-27 19:45:34 +0100

    Billing

--- Billing/models.py
+++ Billing/models.py
@@ -5,6 +5,6 @@ from Reception.models import RoomReservation
 # Create your models here.
 
 class Billing(models.Model):
-    total_amount = models.DecimalField()
+    total_amount = models.IntegerField()
     date = models.DateField()
     room_information = models.ForeignKey(RoomReservation, on_delete=models.CASCADE)
