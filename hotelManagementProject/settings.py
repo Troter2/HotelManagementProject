5a2ab3cc0314ceb7d5041248401a0789e96453ca
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-03-27 16:15:18 +0100

    Added apps

--- hotelManagementProject/settings.py
+++ hotelManagementProject/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Billing',
+    'Cleaning',
+    'Client',
+    'Reception',
+    'Restaurant',
 ]
 
 MIDDLEWARE = [
@@ -117,10 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = "/"
 
 #Add this in your settings.py file:
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
+    BASE_DIR / 'media'
 ]
 
 # Default primary key field type
