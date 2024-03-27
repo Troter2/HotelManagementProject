b94155645c9dd90b9f628f9dbc282e25ad88a079
AdriaCreus <155093465+AdriaCreus@users.noreply.github.com>
2024-03-27 21:26:12 +0100

    added rooms classes

--- Reception/admin.py
+++ Reception/admin.py
@@ -5,3 +5,9 @@ from Reception.models import RoomReservation
 # Register your models here.
 
 admin.site.register(RoomReservation)
+
+from Reception.models import Room, RoomType
+
+# Register your models here.
+admin.site.register(Room)
+admin.site.register(RoomType)
--- Reception/models.py
+++ Reception/models.py
@@ -14,3 +14,16 @@ class RoomReservation(models.Model):
     guests_number = models.IntegerField(default=0)
     # room_type = lo de creus
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+
+class RoomType(models.Model):
+    name = models.CharField(max_length=255)
+    capacity = models.IntegerField()
+    photo = models.ImageField(upload_to='room_photos/')
+    description = models.TextField()
+
+
+class Room(models.Model):
+    room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
+    room_number = models.CharField(max_length=50)
