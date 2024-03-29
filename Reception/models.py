03bf71425de7dfd15fcc1a5d0b4c5bbba38ea333
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-03-29 18:28:49 +0100

    update rooms model.py

--- Reception/models.py
+++ Reception/models.py
@@ -16,10 +16,6 @@ class Room(models.Model):
     room_number = models.CharField(max_length=50)
     is_clean = models.BooleanField(default=False)
 
-class Room(models.Model):
-    room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE, null=True)
-    room_number = models.CharField(max_length=50)
-    is_clean = models.BooleanField(default=False)
 
     def __str__(self):
         return self.room_number
