029bc2c2925f3fae9db4c14c7169b37f4404b9a0
Josep Prades Vidal <jus25gamer@gmail.com>
2024-03-29 17:20:28 +0100

    Make shift view

--- hotelManagementProject/urls.py
+++ hotelManagementProject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from Cleaning.views import cleaner_page, update_room_status
+from Cleaning.views import cleaner_page, update_room_status, cleaner_shift
 from Reception.views import reception_ini, book_room, checkin_form
 from register import views as register
 from Restaurant.views import restaurant_reservation_page
@@ -27,7 +27,8 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('', reception_ini, name="home"),
     path('formulari/', book_room, name="book_room"),
-    path('cleaner', cleaner_page, name='cleaner_page'),
+    path('cleaner/', cleaner_page, name='cleaner_page'),
+    path('shifts/', cleaner_shift, name="shift"),
     path('update_room_status/', update_room_status, name='update_room_status'),
     path('restaurant/reservations/', restaurant_reservation_page, name='restaurant_reservation_page'),
     path('checkin/', checkin_form, name='checkin_form'),
