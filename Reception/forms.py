81806ad204f48d431a65deed625d9281cc76ed72
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-03-30 18:58:31 +0100

    added list booked rooms and validate check in

--- Reception/forms.py
+++ Reception/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from .models import RoomReservation
 
+
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = RoomReservation
         fields = ['guests_name', 'guests_surname', 'DNI', 'guests_email', 'guests_phone', 'guests_number']
+
 
 class CheckIn(forms.ModelForm):
     class Meta:
--- hotelManagementProject/urls.py
+++ hotelManagementProject/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from Cleaning.views import cleaner_page, update_room_status, cleaner_shift
 from Reception.views import reception_ini, book_room, reserved_rooms_view, rooms_view, checkin_form, update_book_arrive
+from User.views import add_guest_view, save_more_guest, save_guest
 from register import views as register
 from Restaurant.views import restaurant_reservation_page
 from django.conf import settings
@@ -36,6 +37,9 @@ urlpatterns = [
     path('restaurant/reservations/', restaurant_reservation_page, name='restaurant_reservation_page'),
     path('checkin/', checkin_form, name='checkin_form'),
     path('reception/reservations/', reserved_rooms_view, name='reserved_rooms_view'),
+    path('reception/add_guest/<int:id>/', add_guest_view, name='add_guest_view'),
+    path('reception/save_more_guest/', save_more_guest, name='save_more_guest'),
+    path('reception/save_guest/<int:id>/', save_guest, name='save_guest'),
     path('rooms/', rooms_view, name='rooms_view'),
 
 
