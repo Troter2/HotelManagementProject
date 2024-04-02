9edf4f8573c0ba80b06dbbb013d25b7d8fd630d0
AdriaCreus <155093465+AdriaCreus@users.noreply.github.com>
2024-04-02 11:34:51 +0200

    Update urls.py

--- hotelManagementProject/urls.py
+++ hotelManagementProject/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from Cleaning.views import cleaner_page, update_room_status, cleaner_shift
 from User.views import add_guest_view, save_more_guest, save_guest, shift_management_page, previous_week, next_week
 from Reception.views import reception_ini, reserved_rooms_view, ocuped_rooms_view, rooms_view, \
-    checkin_form, update_book_arrive, pay_reservation, booking_reservas, filtrar_reservas_check_out,reserve_room,  what_todo
+    checkin_form, update_book_arrive, pay_reservation, booking_filter, filtrar_reservas_check_out,reserve_room,  what_todo
 from register import views as register
 from Restaurant.views import restaurant_reservation_page, restaurant_page
 from django.conf import settings
