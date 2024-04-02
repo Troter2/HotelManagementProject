fa098061cf903457051708b7424089813917e475
Juanca192001 <joancarlesturegano@gmail.com>
2024-04-02 17:03:14 +0200

    Logo, Title and menu corrections

--- Reception/views.py
+++ Reception/views.py
@@ -167,7 +167,7 @@ def what_todo(request):
     return render(request, 'generic/what_to_do.html')
 
 
-def filtrar_reservas_check_out(request):
+def booking_filter_check_out(request):
     # Obtener los parámetros de filtrado desde la URL
     nombre_habitacion = request.GET.get('nombre_habitacion', None)
     fecha = request.GET.get('fecha', None)
