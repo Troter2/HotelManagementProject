fe460cced7c32cdc84f9e670162f38536ef38fe9
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-04-27 19:27:06 +0200

    Added custom user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    DNI = models.CharField(max_length=9)
+    telefono = models.CharField(max_length=11)
--- hotelManagementProject/settings.py
+++ hotelManagementProject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'Reception',
     'Restaurant',
     'User',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
