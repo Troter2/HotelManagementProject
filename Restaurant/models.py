45732db0344d0b71898a8526db0caac96b3edcc8
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-04-29 00:05:14 +0200

    Update models.py

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -66,7 +66,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     active = models.BooleanField(default=True)
-    img = models.ImageField(upload_to='room_photos/')
+    img = models.ImageField(upload_to='item_photo/')
 
 
 class ItemAmount(models.Model):
