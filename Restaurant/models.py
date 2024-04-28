2b3a1605e37e945efd752acacb7bc0a579c37e9a
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-04-28 12:43:42 +0200

    modified restaurant reservation

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,16 +1,8 @@
-
 from django.db import models
 from Reception.models import Room, RoomReservation
 
 
 # Create your models here.
-class RestaurantReservation(models.Model):
-    customers_names = models.CharField(max_length=100)
-    room_number = models.ForeignKey(Room, on_delete=models.CASCADE)
-    customer_contact = models.CharField(max_length=100)
-    number_guests = models.IntegerField(default=0)
-    date_reservation = models.DateField()
-
 
 class Dish(models.Model):
     PRIMER_PLATO_CHOICES = [
@@ -55,14 +47,15 @@ class Dish(models.Model):
     ]
 
     tipus = models.CharField(max_length=20, choices=TIPO_CHOICES)
-    nom = models.CharField(max_length=100,choices=PRIMER_PLATO_CHOICES + SEGUNDO_PLATO_CHOICES + POSTRE_CHOICES + REFRESCO_CHOICES)
+    nom = models.CharField(max_length=100,
+                           choices=PRIMER_PLATO_CHOICES + SEGUNDO_PLATO_CHOICES + POSTRE_CHOICES + REFRESCO_CHOICES)
     descripcio = models.TextField()
     preu = models.DecimalField(max_digits=10, decimal_places=2)
 
-class RestaurantReservation_v2(models.Model):
+
+class RestaurantReservation(models.Model):
     client_name = models.CharField(max_length=100)
     room_reservation = models.ForeignKey(RoomReservation, on_delete=models.CASCADE)
     entrance_hours = models.TimeField()
     date_entrance = models.DateField()
     costumers_number = models.IntegerField(default=0)
-
