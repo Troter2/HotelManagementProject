ec7be0422a42678dca21580dbf4f35c5f2f00213
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-04-29 19:00:50 +0200

    done

--- hotelManagementProject/urls.py
+++ hotelManagementProject/urls.py
@@ -24,7 +24,8 @@ from Reception.views import reception_ini, reserved_rooms_view, ocuped_rooms_vie
     update_book_arrive, pay_reservation, booking_filter, reserve_room, booking_filter_check_out, \
     filtrar_por_numero_reserva, add_lost_item, lost_item_list, update_item_reception
 from register import views as register
-from Restaurant.views import restaurant_reservation_page, restaurant_page, reserved_tables, update_validation, restaurant_reservation_page_uuid, restaurant_validation_page
+from Restaurant.views import restaurant_reservation_page, restaurant_page, reserved_tables, update_validation, \
+    restaurant_reservation_page_uuid, restaurant_validation_page, restaurant_list_items
 from accounts.views import custom_login
 from django.conf import settings
 from django.conf.urls.static import static
@@ -62,6 +63,7 @@ urlpatterns = [
     path('reception/reservations/filter/', booking_filter, name='filtrar_reservas'),
     path('reception/checkout/filter/', booking_filter_check_out, name='booking_filter_check_out'),
     path('restaurant/reservations/<str:uuid>/', restaurant_reservation_page_uuid, name='restaurant_reservation_page_uuid'),
+    path('restaurant/items/', restaurant_list_items, name='restaurant_list_items'),
     path('camarero/reserved', reserved_tables, name='reserved_tables'),
     path('validar_reserva/', update_validation, name='update_validation'),
     path('filtrar_por_numero_reserva/', filtrar_por_numero_reserva, name='filtrar_por_numero_reserva'),
