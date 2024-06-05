c5eb9838ba382b42bd7a5315fd540e66ac433a66
Adrià Creus Nuñez <adriacreus03@gmail.com>
2024-06-05 16:39:17 +0200

    .

--- Billing/views.py
+++ Billing/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from Billing.models import Promotion
+
 
 # Create your views here.
+def list_offers(request):
+    if request.user.has_perm('accountant'):
+        promotions = Promotion.objects.all()
+        return render(request, 'billing/list_offers.html', {'ofertas': promotions})
+    return redirect('home')
