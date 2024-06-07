0a03152fb3248a46fc84352eea93e11f850e1430
Josep Prades Vidal <jus25gamer@gmail.com>
2024-06-07 15:44:25 +0200

    added restaurant invoice

--- Restaurant/admin.py
+++ Restaurant/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from Restaurant.models import RestaurantReservation,Order, ItemAmount, Item
+from Restaurant.models import RestaurantReservation, Order, ItemAmount, Item, RestaurantInvoice
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ admin.site.register(RestaurantReservation)
 admin.site.register(ItemAmount)
 admin.site.register(Item)
 admin.site.register(Order)
+admin.site.register(RestaurantInvoice)
--- Restaurant/models.py
+++ Restaurant/models.py
@@ -58,3 +58,9 @@ class RestaurantReservation(models.Model):
     costumers_number = models.IntegerField(default=0)
     validated = models.BooleanField(default=False)
     order_num = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True)
+
+
+class RestaurantInvoice(models.Model):
+    reserved_room = models.ForeignKey(RoomReservation, on_delete=models.CASCADE, blank=True, null=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    content = models.JSONField()
