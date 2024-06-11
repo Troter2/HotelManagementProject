0aebbb75315db127e4fd6ee1c9ba6541d70d773b
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-06-11 23:19:54 +0200

    Added preview

--- hotelManagementProject/settings.py
+++ hotelManagementProject/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'hotelManagementProject.wsgi.application'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
